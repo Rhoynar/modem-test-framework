67ebfcd50f37ba49601b8d50de40ea4b95e4652d
Harsh <harsh@rhoynar.com>
2017-11-02 15:28:51 -0600

    Force test ordering..

--- tests/modem_checks.py
+++ tests/modem_checks.py
@@ -10,15 +10,15 @@ from mmcli_parser import MMCLIParser
 from results import Results
 
 
-class ModemChecksTestSuite(unittest.TestCase):
-    def test_mmcli_cmd_present(self):
+class ModemChecks(unittest.TestCase):
+    def mmcli_cmd_present(self):
         mmcli = run_cmd('which mmcli')
         if len(mmcli) is 0:
             Results.add_error('which mmcli', 'mmcli is not present. Install modem manager using: sudo apt-get install modemmanager')
         else:
             Results.add_state('MMCLI Exec', mmcli)
 
-    def test_list_modems(self):
+    def list_modems(self):
         # MMCLI not present, skip running other tests.
         mmcli_exec = Results.get_state('MMCLI Exec')
         if mmcli_exec is None:
@@ -32,7 +32,7 @@ class ModemChecksTestSuite(unittest.TestCase):
             Results.add_state('Modem Location', re.search(r'(/org/freedesktop/ModemManager\d/Modem/\d)', mmcli).group(1))
             Results.add_state('Modem Index', re.search(r'/org/freedesktop/ModemManager\d/Modem/(\d)', Results.get_state('Modem Location')).group(1))
 
-    def test_sim_present(self):
+    def sim_present(self):
         # Modem not present, no need to run this test.
         modem_idx = Results.get_state('Modem Index')
         if modem_idx is None:
@@ -45,7 +45,7 @@ class ModemChecksTestSuite(unittest.TestCase):
         else:
             Results.add_error('mmcli -m {}'.format(modem_idx), 'SIM card not found. Fix Insert SIM card and restart modem')
 
-    def test_sim_registered(self):
+    def sim_registered(self):
         # Modem not present, no need to run this test.
         modem_idx = Results.get_state('Modem Index')
         if modem_idx is None:
@@ -62,7 +62,7 @@ class ModemChecksTestSuite(unittest.TestCase):
         else:
             Results.add_error('mmcli -m {}'.format(modem_idx), 'SIM card not registered. Please restart modem manager using: sudo stop modemmanager && sudo start modemmanager')
 
-    def test_get_modem_info(self):
+    def modem_info(self):
         modem_idx = Results.get_state('Modem Index')
         if modem_idx is None:
             return
@@ -80,6 +80,13 @@ class ModemChecksTestSuite(unittest.TestCase):
             assert '3GPP' in res.keys()
         else:
             Results.add_error('mmcli -m {}'.format(modem_idx), 'Error getting/parsing modem info. Contact support with test output.')
+
+    def test_modem_checks(self):
+        self.mmcli_cmd_present()
+        self.list_modems()
+        self.sim_present()
+        self.sim_registered()
+        self.modem_info()
 
 if __name__ == '__main__':
     unittest.main(exit=False)
