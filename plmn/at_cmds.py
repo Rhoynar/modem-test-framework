c3449c91d8070b002698c7935d3efcc33d9ae77d
Harsh <harsh@rhoynar.com>
2017-11-06 14:09:45 -0700

    Print cmd as soon as available. Result is printed later.

--- plmn/at_cmds.py
+++ plmn/at_cmds.py
@@ -29,11 +29,12 @@ class AtCmds():
         modem_idx = Results.get_state('Modem Index')
         assert modem_idx is not None
         cmd = "mmcli -m {} --command='{}' --timeout={}".format(modem_idx, at_cmd, timeout)
+        if at_debug:
+            print "AT command: ", cmd
 
         res = Runner.run_cmd(cmd).strip()
 
         if at_debug:
-            print "AT command: ", cmd
             print "Response: ", res
 
         res = res.replace('\r','|').replace('\n','|')
