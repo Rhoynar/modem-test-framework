3d321f21f39825f0e92ca060babd8e0c3ec354c2
Harsh <harsh@rhoynar.com>
2017-11-07 10:42:03 -0700

    Add simple command tests to regression suite

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -5,6 +5,7 @@ import daemons_check
 import modem_checks
 import python_checks
 import sim_checks
+import simple_cmd_checks
 
 from plmn.results import *
 
@@ -17,6 +18,7 @@ if __name__ == '__main__':
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(modem_checks.ModemChecks))
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(at_checks.AtCmdChecks))
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(sim_checks.SimChecks))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(simple_cmd_checks.SimpleCmdChecks))
 
     # Run the regression suite.
     unittest.TextTestRunner().run(suite)
