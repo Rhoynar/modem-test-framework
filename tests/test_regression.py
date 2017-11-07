5f074a79cc4200c2140f64a1740b09cb6cb8e546
Harsh <harsh@rhoynar.com>
2017-11-07 10:19:00 -0700

    Added regression test suite.
    - Collected all tests into regression test suite that can be run independently.

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,12 +1,25 @@
 import unittest
-from helpers.helpers import *
-from helpers.utils import *
+
+import at_checks
+import daemons_check
+import modem_checks
+import python_checks
+import sim_checks
+
+from plmn.results import *
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(PythonDepsTestSuite))
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(DaemonsChecksTestSuite))
-    unittest.TextTestRunner().run(suite)
-    SystemChecksErrors.print_errors()
-    Results.print_config()
 
+    # Add all regression test-cases to this test-suite.
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(python_checks.PythonChecks))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(daemons_check.DaemonChecks))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(modem_checks.ModemChecks))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(at_checks.AtCmdChecks))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(sim_checks.SimChecks))
+
+    # Run the regression suite.
+    unittest.TextTestRunner().run(suite)
+
+    # Print final system state.
+    Results.print_results()
